34b99128823c144a96e8c33b3c343bdab9c75b32
Franklin Dambra <franklindambra@Franklins-MacBook-Pro.local>
2025-02-12 10:59:18 -0600

    meta update

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Frank | Personal Portfolio",
-  description: "Ricardo is a full-stack developer with 8 years of experience.",
+  description: "Franklin is a full-stack developer with 8 years of experience.",
 };
 
 export default function RootLayout({
